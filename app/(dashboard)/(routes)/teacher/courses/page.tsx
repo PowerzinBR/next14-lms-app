38a886d0ee222a825ff74ae270b608a0541edbf8
PowerzinBR <luizotavionunesfelix@gmail.com>
2024-06-07 12:36:37 -0300

    23: course data table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,29 @@
-import Link from "next/link";
+import { redirect } from "next/navigation"
+import { auth } from "@clerk/nextjs/server";
 
 import { Button } from "@/components/ui/button";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { db } from "@/lib/db";
+const CoursesPage = async () => {
+  const { userId } = auth();
 
-const CoursesPage = () => {
+  if (!userId) {
+    return redirect("/")
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+
   return ( 
     <div className="p-6">
-      <Link href="/teacher/create">   
-        <Button>
-          Novo curso
-        </Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div> 
   );
 }
