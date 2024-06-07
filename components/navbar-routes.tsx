3fd387168486a6d0ea27b821a8b675740809ec02
PowerzinBR <luizotavionunesfelix@gmail.com>
2024-06-07 13:50:31 -0300

    27: admin protection

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { isTeacher } from "@/lib/teacher";
 import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
+  const { userId } = useAuth();
+    
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
@@ -30,13 +33,14 @@ export const NavbarRoutes = () => {
               Sair
             </Button>
           </Link>
-        ) : (
+        ) : isTeacher(userId!) ? (
+          
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Painel do Professor
             </Button>
           </Link>
-        )}
+        ) : null}
         <UserButton
           afterSignOutUrl="/"
         />
